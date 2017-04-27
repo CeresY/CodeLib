3e55aedd7ae41461cd31c1fcb6dd5a0cef32067a
CeresY <oak1990@163.com>
2017-04-27 13:25:28 +0800

    获取当月最后一天，当日是周几

--- src/corejava/base/DateTest.java
+++ src/corejava/base/DateTest.java
@@ -13,17 +13,40 @@ import org.junit.Test;
  * @date 2017年4月25日-上午10:41:09
  */
 public class DateTest {
-
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd"); 
+	
 	/**
-	 * 获取当月最后一天
+	 * 获取当月最后一天和第一天
+	 * @throws ParseException 
 	 */
 	@Test
-	public void lastDay() {
-		Calendar cal = Calendar.getInstance();
-		cal.setTimeInMillis(new Date().getTime());
-		//cal.set(currentDate.getDate(), cal.getActualMaximum(currentDate.getDate()));
-		/*SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		System.out.println(sdf.format(cal.getTime()));*/
-		System.out.println(Calendar.DATE);
+	public void lastDay() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); 
+		Date date = sdf.parse("2017-07-01");
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		
+		Date firstDayOfMonth = calendar.getTime();  
+		System.out.println(sdf.format(firstDayOfMonth));
+		
+		calendar.add(Calendar.MONTH, 1);
+		calendar.add(Calendar.DAY_OF_MONTH, -1);
+		Date lastDayOfMonth = calendar.getTime();
+		System.out.println(sdf.format(lastDayOfMonth));
+	}
+	
+	/**
+	 * 星期几
+	 * @throws ParseException 
+	 */
+	@Test
+	public void dayOfWeek() throws ParseException {
+		Date date = sdf.parse("20170424");
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		int day = calendar.get(Calendar.DAY_OF_WEEK);
+		System.out.println(day);
 	}
 }
