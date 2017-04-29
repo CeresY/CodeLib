01190e4d7e61dfb195f7f4db26d80c2133ced693
CeresY <oak1990@163.com>
2017-04-29 11:24:04 +0800

    fix bug

--- src/corejava/math/MathT.java
+++ src/corejava/math/MathT.java
@@ -1,14 +1,9 @@
 package corejava.math;
 
+import org.junit.Test;
+
 public class MathT {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		scope();
-	}
-	
 	public static void scope() {
 		System.out.println(Float.MAX_VALUE);
 	}
@@ -19,6 +14,12 @@ public class MathT {
 			double d = Math.random();
 			System.out.println("d=" + d + "\td*99=" + d*99);
 		}
+	}
+	
+	@Test
+	public void test() {
+		int i = 5;
+		System.err.println("整除:" + i/4);
 	}
 
 }
