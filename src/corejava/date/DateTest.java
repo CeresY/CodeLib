823762d9294338f593c8809fa78a93ed8e6c3d7a
CeresY <asuka.y@qq.com>
2017-06-04 16:12:54 +0800

    日期对比大小

---
+++ src/corejava/date/DateTest.java
@@ -0,0 +1,17 @@
+package corejava.date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class DateTest {
+	@Test
+	public void compare() throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMM");
+		Date d1 = format.parse("201702");
+		Date d2 = format.parse("201704");
+		System.out.println(d1.compareTo(d2));//-1
+	}
+}
